5d282371215431bbbb04b5b195afb8d8fe817c03
JoTerrance <gorka.zgz@gmail.com>
2025-10-12 00:39:40 +0200

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 sudo apt update
-# esto seguro que funciona
+# curl, pip, ansible
 sudo apt install curl python3-pip ansible -y
+# install nix package manager 
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
 if [ -z "$CODESPACE_NAME" ]; then 
   ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
 else
